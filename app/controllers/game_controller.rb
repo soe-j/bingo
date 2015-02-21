7d2135fde92950623f5a38a1ab53df7c0ada31dd
soe-j <soeda@val.co.jp>
2015-02-21 13:56:31 +0900

    generate game controller and view

---
+++ app/controllers/game_controller.rb
@@ -0,0 +1,10 @@
+class GameController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'game/index'
+
+  get 'game/create'
+
+  get 'game/show'
+
   root 'top#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
