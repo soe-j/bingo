039bc58c6033a940bd7a97d28cbde48565034e30
soe-j <soeda@val.co.jp>
2015-02-21 15:02:46 +0900

    game#create, #index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,9 +8,17 @@ class GamesController < ApplicationController
   end
 
   def create
-    @game = Game.new
+    @game = Game.new(game_params)
+    @game.save
+    redirect_to games_path
   end
 
   def show
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name)
   end
 end
