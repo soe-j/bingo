e8854ad5e7b4d5018712c638570eb85aa1146439
soe-j <soeda@val.co.jp>
2015-04-08 15:56:32 +0900

    振ってるの検知するjs

---
+++ app/assets/javascripts/handle.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  window.addEventListener('devicemotion', function (e) {
+    var x = e.accelerationIncludingGravity.x;
+    var y = e.accelerationIncludingGravity.y;
+    var z = e.accelerationIncludingGravity.z;
+
+    // 3方向加速度の合計の絶対値が一定以上のとき
+    if (Math.abs(x+y+z) > 50) {
+      alert("振ってるよ");
+    }
+  }, true);
+});
