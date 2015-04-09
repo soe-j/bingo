2e3fafc38ee0952e2e49624b7b6b18b25001120c
soe-j <soeda@val.co.jp>
2015-04-10 01:46:01 +0900

    controllerとaction毎に動作するjsを分けた。ついでにjQuery.readyを省略して書いた

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,29 +1,33 @@
-$(document).ready(function(){
-  setInterval(function(){
-    $.getJSON(location.href + '.json', set_balls);
-  }, 2000);
+$(function(){
 
-  function set_balls(new_balls){
+  if ($('body').data('controller') == 'games' && $('body').data('action') == 'show'){
+    setInterval(function(){
+      $.getJSON(location.href + '.json', set_balls);
+    }, 2000);
 
-    // ローカルストレージから古いデータを取得
-    var str = localStorage.getItem("balls");
-    old_balls = JSON.parse(str);
+    function set_balls(new_balls){
 
-    // 比較
-    var balls = $.grep(new_balls, function(n,i){
-      return ($.inArray(n, old_balls) == -1);
-    });
+      // ローカルストレージから古いデータを取得
+      var str = localStorage.getItem("balls");
+      old_balls = JSON.parse(str);
 
-    // 差分だけページに追加
-    var content = '';
-    var i;
-    for(i = 0; i < balls.length; i++) {
-      content += "\n<img src='/assets/numbers/" + balls[i] + ".png'>";
+      // 比較
+      var balls = $.grep(new_balls, function(n,i){
+        return ($.inArray(n, old_balls) == -1);
+      });
+
+      // 差分だけページに追加
+      var content = '';
+      var i;
+      for(i = 0; i < balls.length; i++) {
+        content += "\n<img src='/assets/numbers/" + balls[i] + ".png'>";
+      }
+      $('#numbers_list').append(content);
+
+      // ローカルストレージに保存
+      var json_text = JSON.stringify(new_balls);
+      localStorage.setItem("balls", json_text);
     }
-    $('#numbers_list').append(content);
-
-    // ローカルストレージに保存
-    var json_text = JSON.stringify(new_balls);
-    localStorage.setItem("balls", json_text);
   }
+
 });
