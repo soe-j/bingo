9de1c01fb1a29509bead94a3bb8dd94b0eba6d78
soe-j <soeda@val.co.jp>
2015-04-11 23:25:16 +0900

    降った時にビンゴをひく。5000msに１回だけ送信できる。

--- app/assets/javascripts/handle.js
+++ app/assets/javascripts/handle.js
@@ -1,12 +1,22 @@
 $(function(){
-  window.addEventListener('devicemotion', function (e) {
-    var x = e.accelerationIncludingGravity.x;
-    var y = e.accelerationIncludingGravity.y;
-    var z = e.accelerationIncludingGravity.z;
+  if ($('body').data('controller') == 'games' && $('body').data('action') == 'handle'){
 
-    // 3方向加速度の合計の絶対値が一定以上のとき
-    if (Math.abs(x+y+z) > 49) {
-      alert("振ってるよ");
+    var update_flag = 1;
+
+    window.addEventListener('devicemotion', function (e) {
+      var x = e.accelerationIncludingGravity.x;
+      var y = e.accelerationIncludingGravity.y;
+      var z = e.accelerationIncludingGravity.z;
+
+      // 3方向加速度の合計の絶対値が一定以上のとき
+      if (Math.abs(x+y+z) > 48 && update_flag == 1) {
+        update_flag = 0;
+        update_bingo();
+        wait(5000);
+        update_flag = 1;
+      }
+    }, true);
+
     $('#handle').click(update_bingo);
 
     function update_bingo() {
@@ -18,6 +28,15 @@ $(function(){
         async: false
       });
     }
+
+    function wait( timeWait ) {
+      var timeStart = new Date().getTime();
+      var timeNow = new Date().getTime();
+      while( timeNow < (timeStart + timeWait ) ) {
+        timeNow = new Date().getTime();
+      }
+      return;
     }
-  }, true);
+
+  }
 });
