f83cac7a89badb2c50f05e4940c4c9d49b036c7d
soe-j <soeda@val.co.jp>
2015-04-11 23:23:49 +0900

    ajaxを使ってビンゴをひく実装

--- app/assets/javascripts/handle.js
+++ app/assets/javascripts/handle.js
@@ -7,6 +7,17 @@ $(function(){
     // 3方向加速度の合計の絶対値が一定以上のとき
     if (Math.abs(x+y+z) > 49) {
       alert("振ってるよ");
+    $('#handle').click(update_bingo);
+
+    function update_bingo() {
+      var current_url = location.href;
+      var target_url = current_url.replace('handle','');
+      $.ajax({
+        type: "PATCH",
+        url: target_url,
+        async: false
+      });
+    }
     }
   }, true);
 });
